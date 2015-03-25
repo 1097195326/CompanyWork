e3d85c34c18818af9dbaf580e9c2f2134548d01a
张洪星 <zhanghongxing@zhanghongxingdeMacBook-Pro.local>
2015-03-25 18:26:35 +0800

    cc

---
+++ SwampAttack/proj.android/obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/CCEventListenerTouch.o.d
@@ -0,0 +1,158 @@
+obj/local/armeabi/objs-debug/cocos2dx_internal_static/base/CCEventListenerTouch.o: \
+ jni/../../cocos2d/cocos/./base/CCEventListenerTouch.cpp \
+ jni/../../cocos2d/cocos/./base/CCEventListenerTouch.h \
+ jni/../../cocos2d/cocos/./base/CCEventListener.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/functional \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/memory \
+ jni/../../cocos2d/cocos/./platform/CCPlatformMacros.h \
+ jni/../../cocos2d/cocos/./base/ccConfig.h \
+ jni/../../cocos2d/cocos/./platform/CCPlatformConfig.h \
+ jni/../../cocos2d/cocos/./platform/CCPlatformDefine.h \
+ jni/../../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/log.h \
+ jni/../../cocos2d/cocos/./base/CCRef.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
+ jni/../../cocos2d/cocos/./base/CCEventDispatcher.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/unordered_map \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/set \
+ jni/../../cocos2d/cocos/./base/CCEvent.h \
+ jni/../../cocos2d/cocos/./platform/CCStdC.h \
+ jni/../../cocos2d/cocos/./platform/android/CCStdC-android.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/math.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/cassert \
+ jni/../../cocos2d/cocos/./base/CCEventTouch.h \
+ jni/../../cocos2d/cocos/./base/CCTouch.h \
+ jni/../../cocos2d/cocos/./math/CCGeometry.h \
+ jni/../../cocos2d/cocos/./base/ccMacros.h \
+ jni/../../cocos2d/cocos/./base/CCConsole.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/thread \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/map \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/mutex \
+ jni/../../cocos2d/cocos/./base/ccRandom.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/random \
+ jni/../../cocos2d/cocos/./math/CCMath.h \
+ jni/../../cocos2d/cocos/./math/Vec2.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
+ jni/../../cocos2d/cocos/./math/CCMathBase.h \
+ jni/../../cocos2d/cocos/./math/Vec2.inl \
+ jni/../../cocos2d/cocos/./math/Vec2.h \
+ jni/../../cocos2d/cocos/./math/Vec3.h \
+ jni/../../cocos2d/cocos/./math/Vec3.inl \
+ jni/../../cocos2d/cocos/./math/Vec3.h \
+ jni/../../cocos2d/cocos/./math/Mat4.h \
+ jni/../../cocos2d/cocos/./math/Vec4.h \
+ jni/../../cocos2d/cocos/./math/Vec4.inl \
+ jni/../../cocos2d/cocos/./math/Mat4.inl \
+ jni/../../cocos2d/cocos/./math/Vec4.h \
+ jni/../../cocos2d/cocos/./math/Mat4.h \
+ jni/../../cocos2d/cocos/./math/Quaternion.h \
+ jni/../../cocos2d/cocos/./math/Quaternion.inl
+
+jni/../../cocos2d/cocos/./base/CCEventListenerTouch.h:
+
+jni/../../cocos2d/cocos/./base/CCEventListener.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/functional:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/memory:
+
+jni/../../cocos2d/cocos/./platform/CCPlatformMacros.h:
+
+jni/../../cocos2d/cocos/./base/ccConfig.h:
+
+jni/../../cocos2d/cocos/./platform/CCPlatformConfig.h:
+
+jni/../../cocos2d/cocos/./platform/CCPlatformDefine.h:
+
+jni/../../cocos2d/cocos/./platform/android/CCPlatformDefine-android.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+jni/../../cocos2d/cocos/./base/CCRef.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
+
+jni/../../cocos2d/cocos/./base/CCEventDispatcher.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/unordered_map:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/set:
+
+jni/../../cocos2d/cocos/./base/CCEvent.h:
+
+jni/../../cocos2d/cocos/./platform/CCStdC.h:
+
+jni/../../cocos2d/cocos/./platform/android/CCStdC-android.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/math.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/cassert:
+
+jni/../../cocos2d/cocos/./base/CCEventTouch.h:
+
+jni/../../cocos2d/cocos/./base/CCTouch.h:
+
+jni/../../cocos2d/cocos/./math/CCGeometry.h:
+
+jni/../../cocos2d/cocos/./base/ccMacros.h:
+
+jni/../../cocos2d/cocos/./base/CCConsole.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/thread:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/map:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/mutex:
+
+jni/../../cocos2d/cocos/./base/ccRandom.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/random:
+
+jni/../../cocos2d/cocos/./math/CCMath.h:
+
+jni/../../cocos2d/cocos/./math/Vec2.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
+
+jni/../../cocos2d/cocos/./math/CCMathBase.h:
+
+jni/../../cocos2d/cocos/./math/Vec2.inl:
+
+jni/../../cocos2d/cocos/./math/Vec2.h:
+
+jni/../../cocos2d/cocos/./math/Vec3.h:
+
+jni/../../cocos2d/cocos/./math/Vec3.inl:
+
+jni/../../cocos2d/cocos/./math/Vec3.h:
+
+jni/../../cocos2d/cocos/./math/Mat4.h:
+
+jni/../../cocos2d/cocos/./math/Vec4.h:
+
+jni/../../cocos2d/cocos/./math/Vec4.inl:
+
+jni/../../cocos2d/cocos/./math/Mat4.inl:
+
+jni/../../cocos2d/cocos/./math/Vec4.h:
+
+jni/../../cocos2d/cocos/./math/Mat4.h:
+
+jni/../../cocos2d/cocos/./math/Quaternion.h:
+
+jni/../../cocos2d/cocos/./math/Quaternion.inl:
---
+++ SwampAttack/proj.android/obj/local/armeabi/objs-debug/cpufeatures/cpu-features.o.d
@@ -0,0 +1,176 @@
+obj/local/armeabi/objs-debug/cpufeatures/cpu-features.o: \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/android/cpufeatures/cpu-features.c \
+ /Users/zhanghongxing/work/android-ndk-r10b/sources/android/cpufeatures/cpu-features.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/dlfcn.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/errno.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/fcntl.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/fcntl.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/fcntl.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/signal.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sched.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/system_properties.h
+
+/Users/zhanghongxing/work/android-ndk-r10b/sources/android/cpufeatures/cpu-features.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/dlfcn.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/fcntl.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/zhanghongxing/work/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/system_properties.h:
