eb430c1be5a163cbbe9d44f0abede71e6ae3be7a
oceantech02 <oceantech02@oceantech02deMac-mini.local>
2015-05-07 19:13:14 +0800

    2015.05.07 02

--- SwampAttack/Classes/ScrollController.cpp
+++ SwampAttack/Classes/ScrollController.cpp
@@ -12,6 +12,7 @@
 ScrollController::ScrollController():
 m_vertical(false),
 m_horizontal(false),
+m_undulate(true),
 m_centerPoint(leftTop)
 {
     
@@ -23,44 +24,69 @@ ScrollController::~ScrollController()
 }
 void ScrollController::update(float data)
 {
-    if (false) {
-        
-    }
-    M_Vec2f force;
-    if (m_offSet.x < m_viewWidth - m_contentWidth)
-    {
-        force.x = (m_viewWidth - m_contentWidth - m_offSet.x) * 0.5;
-        
-    }else if (m_offSet.x > 0.0)
-    {
-        force.x = (0.0 - m_offSet.x) * 0.5;
-    }
-    if (m_centerPoint == leftTop) {
-        if (m_offSet.y > m_contentHeight - m_viewHeight)
-        {
-            force.y = (m_contentHeight - m_viewHeight - m_offSet.y) * 0.5;
-        }else if (m_offSet.y < 0.0)
-        {
-            force.y = (0.0 - m_offSet.y) * 0.5;
-        }
-    }
-    if (m_centerPoint == leftBottom) {
-        if (m_offSet.y < m_viewHeight - m_contentHeight)
-        {
-            force.y = (m_viewHeight - m_contentHeight - m_offSet.y) * 0.5;
-        }else if (m_offSet.y > 0.0)
-        {
-            force.y = (0.0 - m_offSet.y) * 0.5;
-        }
-    }
-    
     M_Vec2f normalForce = multMV(m_velocity, -0.1);
-    
     m_velocity.add(normalForce);
 //    printf("m_velocity x:%f,y:%f \n",m_velocity.x,m_velocity.y);
     M_Vec2f change = multMV(m_velocity, data);
     m_offSet.add(change);
-    m_offSet.add(force);
+    
+    if (m_undulate)
+    {
+        M_Vec2f force;
+        if (m_offSet.x < m_viewWidth - m_contentWidth)
+        {
+            force.x = (m_viewWidth - m_contentWidth - m_offSet.x) * 0.5;
+        }else if (m_offSet.x > 0.0)
+        {
+            force.x = (0.0 - m_offSet.x) * 0.5;
+        }
+        if (m_centerPoint == leftTop) {
+            if (m_offSet.y > m_contentHeight - m_viewHeight)
+            {
+                force.y = (m_contentHeight - m_viewHeight - m_offSet.y) * 0.5;
+            }else if (m_offSet.y < 0.0)
+            {
+                force.y = (0.0 - m_offSet.y) * 0.5;
+            }
+        }
+        if (m_centerPoint == leftBottom) {
+            if (m_offSet.y < m_viewHeight - m_contentHeight)
+            {
+                force.y = (m_viewHeight - m_contentHeight - m_offSet.y) * 0.5;
+            }else if (m_offSet.y > 0.0)
+            {
+                force.y = (0.0 - m_offSet.y) * 0.5;
+            }
+        }
+        m_offSet.add(force);
+    }else
+    {
+        if (m_offSet.x < m_viewWidth - m_contentWidth)
+        {
+            m_offSet.x = (m_viewWidth - m_contentWidth);
+        }else if (m_offSet.x > 0.0)
+        {
+            m_offSet.x = 0.0;
+        }
+        if (m_centerPoint == leftTop) {
+            if (m_offSet.y > m_contentHeight - m_viewHeight)
+            {
+                m_offSet.y = m_contentHeight - m_viewHeight;
+            }else if (m_offSet.y < 0.0)
+            {
+                m_offSet.y = 0.0;
+            }
+        }
+        if (m_centerPoint == leftBottom) {
+            if (m_offSet.y < m_viewHeight - m_contentHeight)
+            {
+                m_offSet.y = m_viewHeight - m_contentHeight;
+            }else if (m_offSet.y > 0.0)
+            {
+                m_offSet.y = 0.0;
+            }
+        }
+    }
 //    printf("m_offSet x:%f,y:%f \n",m_offSet.x,m_offSet.y);
 }
 void ScrollController::updateOffSet(float ox, float oy)
