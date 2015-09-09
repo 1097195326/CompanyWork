024fad417baa1a31ee85b2478caff16bc9fa8d60
oceantech02 <oceantech02@oceantech02deMac-mini.local>
2015-09-09 11:09:29 +0800

    2015.09.09 01

---
+++ SwampAttack/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/GameObject.o.d
@@ -0,0 +1,4 @@
+obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/GameObject.o: \
+ jni/../../Classes/GameObject.cpp jni/../../Classes/GameObject.h
+
+jni/../../Classes/GameObject.h:
---
+++ SwampAttack/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/State.o.d
@@ -0,0 +1,4 @@
+obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/State.o: \
+ jni/../../Classes/State.cpp jni/../../Classes/State.h
+
+jni/../../Classes/State.h:
