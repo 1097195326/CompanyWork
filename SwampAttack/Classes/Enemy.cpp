300c759dada2b61694236c996f1115bc03283776
1097195326 <1097195326@qq.com>
2015-09-22 16:48:36 +0800

    2015.09.22 02

--- SwampAttack/Classes/Enemy.cpp
+++ SwampAttack/Classes/Enemy.cpp
@@ -401,8 +401,8 @@ void Enemy::attackCall()
                                0,
                                1,
                                t_house,
-                               m_point - Vec2(m_width * 0.5, 0) + Vec2(0, m_height * 0.7),
-                               m_targetPoint + Vec2(0,m_height * 0.7),
+                               m_point - Vec2(m_width * 0.5, 0) + Vec2(0, m_height * 0.65),
+                               m_targetPoint + Vec2(0,m_height * 0.65),
                                m_bulletModelId
                                );
             BulletManager::getInstance()->fire(bp);
